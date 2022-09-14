e1f92f960bb48e12c9f63a99d9105fa51d364108
VietDQ <vietdq@VietDQs-iMac.local>
2022-09-14 20:54:41 +0700

    add assets

--- assets/svg/index.js
+++ assets/svg/index.js
@@ -1,5 +1,17 @@
 
+import add_photo from "./add_photo.svg"
+import back from "./back.svg"
+import bottom_linear from "./bottom_linear.svg"
+import camera from "./camera.svg"
+import close from "./close.svg"
+import top_linear from "./top_linear.svg"
+
 export const SVG = {
-  
+    add_photo,
+    back,
+    bottom_linear,
+    camera,
+    close,
+    top_linear
 }
 
